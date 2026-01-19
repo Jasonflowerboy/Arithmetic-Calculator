42f7ad044d69db8e1fd2b726df3b050c35f426bc
Jasonmani Lingkhim Rai <jasonraidota@gmail.com>
2026-01-19 23:57:02 +0530

    added sound onclick and fixed blue user-select issue

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
 const display = document.getElementById("display");
+const clicksound = document.getElementById("click-sound");
+
+function playClick()
+{
+    clicksound.currentTime = 0.2; /*Found out my audio is a bit delayed so we start from 0.2 instead of 0 everytime*/
+    clicksound.play();  
+}
 
 function appendToDisplay(input)
 {
+    playClick();
     display.value += input;
 }
 
 function clearDisplay()
 {
+    playClick();
     display.value ="";
 }
 
 function calculate()
-{
+{   playClick();
     try{display.value = eval(display.value);
 
     }
