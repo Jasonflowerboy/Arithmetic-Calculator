bd4f70c38c1c9281eca0fc6792e8781cb45551f6
Jasonmani Lingkhim Rai <jasonraidota@gmail.com>
2026-01-19 23:24:00 +0530

    third update for the zoom.

--- script.js
+++ script.js
@@ -21,3 +21,17 @@ function calculate()
         display.value = "sike - error";
     }
 }
+
+
+
+
+/*Disabling Zoom on mobile browser */
+let lastTouchEnd = 0;
+
+document.addEventListener("touchend", function (e) {
+    const now = new Date().getTime();
+    if (now - lastTouchEnd <= 300) {
+        e.preventDefault();
+    }
+    lastTouchEnd = now;
+}, false);
